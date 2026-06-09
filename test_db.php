0d5614c30c03a096876e328bfcb51f5035912014
Dharma Situmorang <dharmabakti1202@gmail.com>
2026-06-09 15:05:53 +0700

    Chore: Remove leftover scratch and test files

--- test_db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = new PDO("sqlsrv:Server=192.168.1.189;Database=dbtruck", "sa", "123");
-$stmt = $db->query("SELECT TOP 1 * FROM TradoLuarDetailMdn");
-print_r($stmt->fetch(PDO::FETCH_ASSOC));
